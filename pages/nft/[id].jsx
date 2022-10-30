06f8340bc2d9b624f14ac0468a8b51becd1072fb
nasir456244 <arturlegendflame@gmail.com>
2022-10-30 16:05:19 +1000

    updated dependacy

--- pages/nft/[id].jsx
+++ pages/nft/[id].jsx
@@ -16,7 +16,7 @@ const NFTDropPage = () => {
                 <div className='bg-gradient-to-br from-yellow-400 
                     to-purple-600 p-2 rounded-xl'>
                     <Image className='w-44 bg-[#FAE6B2] rounded-xl object-cover 
-                        lg:h-96 lg:w-72' src={bidaar} />
+                        lg:h-96 lg:w-72' alt='bidaarLogo' src={bidaar} />
                 </div>
                 <div className='text-center p-5 space-y-2'>
                     <h1 className='text-xl font-bold text-white'>BIDAAR Nfts</h1>
@@ -40,15 +40,15 @@ const NFTDropPage = () => {
 
             </div>
             <hr className='my-2 border' />
-            {address && <p className='text-center text-sm text-rose-400'>You're logged in with {address?.slice(0,5) + '...' + address.slice(38,42)} </p>}
+            {address && <p className='text-center text-sm text-rose-400'>You&apos;re logged in with {address?.slice(0,5) + '...' + address.slice(38,42)} </p>}
 
 
 
             <div className='mt-10 flex flex-1 flex-col items-center space-y-6
                 text-center lg:space-y-0 lg:justify-center'>
-                <Image className='w-80 object-cover pb-10 lg:h-40' src={bidaar} />
+                <Image className='w-80 object-cover pb-10 lg:h-40' src={bidaar} alt="bidaarBanner"/>
                 <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>BIDAAR is Life | NFT Drop </h1>
-                <p className='pt-2 text-xl text-green-500'> 13 / 21 NFT's claimed</p>
+                <p className='pt-2 text-xl text-green-500'> 13 / 21 NFT&apos;s claimed</p>
             </div>
 
             <button className='mt-10 h-16 bg-red-600 w-full text-white 
