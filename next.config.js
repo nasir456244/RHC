60ad9408c7807ee7c35ecfe876671e5cdda785d4
nasir456244 <arturlegendflame@gmail.com>
2022-11-09 14:10:45 +1000

    reset

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.sanity.io', 'cdn.dribbble.com', 'gateway.ipfscdn.io']
+    domains: ['cdn.sanity.io', 'cdn.dribbble.com']
   }
 }
 
